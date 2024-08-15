1729cfea6999ca29c2d59ddd4a35ff138ad80dd2
Sharad Jadhav <60263979+jadhavsharad@users.noreply.github.com>
2024-08-15 22:42:23 +0530

    Update webhooks.jsx

--- app/routes/webhooks.jsx
+++ app/routes/webhooks.jsx
@@ -4,15 +4,11 @@ import db from "../db.server";
 export const action = async ({ request }) => {
   const { topic, shop, session, admin } = await authenticate.webhook(request);
 
-  if (!admin && topic !== "SHOP_REDACT") {
+  if (!admin) {
     // The admin context isn't returned if the webhook fired after a shop was uninstalled.
-    // The SHOP_REDACT webhook will be fired up to 48 hours after a shop uninstalls the app.
-    // Because of this, no admin context is available.
     throw new Response();
   }
 
-  // The topics handled here should be declared in the shopify.app.toml.
-  // More info: https://shopify.dev/docs/apps/build/cli-for-apps/app-configuration
   switch (topic) {
     case "APP_UNINSTALLED":
       if (session) {
@@ -20,9 +16,21 @@ export const action = async ({ request }) => {
       }
 
       break;
-    case "CUSTOMERS_DATA_REQUEST":
+      case "CUSTOMERS_DATA_REQUEST":
+      console.log("customer data requested");
+      return new Response(null, { status: 200 });
+      
+
     case "CUSTOMERS_REDACT":
+      console.log("customer erasure requested")
+      return new Response(null, { status: 200 });
+
     case "SHOP_REDACT":
+      console.log("shop erasure requested")
+      if (session) {
+        await db.session.deleteMany({ where: { shop } });
+      }
+      return new Response(null, { status: 200 });
     default:
       throw new Response("Unhandled webhook topic", { status: 404 });
   }
